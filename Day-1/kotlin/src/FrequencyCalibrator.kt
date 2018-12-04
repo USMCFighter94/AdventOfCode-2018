9722a2fbb89a7851c529a7178c795e5c324e0632
Colin Brevitz <verlander94@gmail.com>
2018-12-03 22:24:24 -0500

    Adjust function to complete Day 1, Part 2

--- Day-1/kotlin/src/FrequencyCalibrator.kt
+++ Day-1/kotlin/src/FrequencyCalibrator.kt
@@ -1,16 +1,43 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    var total = 0
     val inputFile = args.takeIf { it.isNotEmpty() }?.get(0)?.takeIf { it.isNotEmpty() } ?: "inputs.txt"
+    val file = File(inputFile)
+    val frequencyAdjustments = file.readLines()
+    val frequenciesSeen = arrayListOf(0)
 
-    File(inputFile).forEachLine {
-        when (it[0]) {
-            '+' -> total += it.substring(1, it.length).toInt()
-            '-' -> total -= it.substring(1, it.length).toInt()
-            else -> System.out.println("Invalid input $it")
+    var frequency = 0
+    var foundDuplicate = false
+
+    while (!foundDuplicate) {
+        frequencyAdjustments.forEach {
+            when (it[0]) {
+                '+' -> {
+                    frequency += it.substring(1, it.length).toInt()
+
+                    if (frequenciesSeen.contains(frequency)) {
+                        System.out.println("Found duplicate frequency $frequency")
+                        foundDuplicate = true
+                        return@forEach
+                    }
+
+                    frequenciesSeen.add(frequency)
+                }
+                '-' -> {
+                    frequency -= it.substring(1, it.length).toInt()
+
+                    if (frequenciesSeen.contains(frequency)) {
+                        System.out.println("Found duplicate frequency $frequency")
+                        foundDuplicate = true
+                        return@forEach
+                    }
+
+                    frequenciesSeen.add(frequency)
+                }
+                else -> System.out.println("Invalid input $it")
+            }
         }
     }
 
-    System.out.println("Calibrated frequency is $total")
+    System.out.println("Calibrated frequency is $frequency")
 }
